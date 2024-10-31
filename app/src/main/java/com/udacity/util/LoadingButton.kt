a07318e8c9634abf4e40b08fdaab91e0983b5bea
Ali Al-Derawi <aalderawi2013@students.iugaza.edu.ps>
2024-10-31 21:22:53 +0300

    feat: Enable setting textStyle for LoadingButton from XML

--- app/src/main/java/com/udacity/util/LoadingButton.kt
+++ app/src/main/java/com/udacity/util/LoadingButton.kt
@@ -33,23 +33,25 @@ class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private var buttonIdleColor = 0
-    private var buttonLoadingColor = 0
-    private var circleLoadingColor = 0
     private var widthSize = 0
     private var heightSize = 0
     private var textWidth = 0f
     private var buttonString = ""
-    private var buttonTextColor = 0
     private var progressWidth = 0f
     private var progressCircle = 0f
     private var circleXOffset = 0f
-    private var cornerRadius = 0f
-    private var buttonTextSize = 20f
     private var paint: Paint
     private var valueAnimator = ValueAnimator()
-    private var isTextCaps = false
 
+    // TODO : Allow Customization of Button from Code
+    var buttonTextColor = 0
+    var cornerRadius = 0f
+    var buttonTextSize = 20f
+    var isTextCaps = false
+    var textStyle = 0
+    private var buttonIdleColor = 0
+    private var buttonLoadingColor = 0
+    private var circleLoadingColor = 0
 
     private var buttonStateObservable: ButtonStatus by Delegates.observable<ButtonStatus>(
         ButtonStatus.IDLE
@@ -70,12 +72,20 @@ class LoadingButton @JvmOverloads constructor(
             cornerRadius = getDimension(R.styleable.LoadingView_cornerRadius, 32f)
             buttonTextSize = getDimension(R.styleable.LoadingView_textSize, 20f)
             isTextCaps = getBoolean(R.styleable.LoadingView_textAllCaps, false)
+            textStyle = use {
+                it.getInt(R.styleable.LoadingView_textStyle, 0) // Default to "normal"
+            }
         }
         paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             style = Paint.Style.FILL
             textAlign = Paint.Align.CENTER
-            typeface = Typeface.DEFAULT_BOLD
             textSize = buttonTextSize
+        }
+        paint.typeface = when (textStyle) {
+            1 -> Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
+            2 -> Typeface.create(Typeface.DEFAULT, Typeface.ITALIC)
+            3 -> Typeface.create(Typeface.DEFAULT, Typeface.BOLD_ITALIC)
+            else -> Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
         }
         buttonString = resources.getString(R.string.button_download).let {
             if (isTextCaps) it.uppercase() else it
