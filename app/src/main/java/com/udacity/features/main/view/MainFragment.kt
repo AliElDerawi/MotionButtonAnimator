ae949838711f4a0b15e42a283263feda9c1ce980
Ali Al-Derawi <aalderawi2013@students.iugaza.edu.ps>
2025-01-06 20:14:06 +0200

    feat: Use the (with) receiver function in MainFragment to omit the need to write ViewModel and Binding name when using its members or functions

--- app/src/main/java/com/udacity/features/main/view/MainFragment.kt
+++ app/src/main/java/com/udacity/features/main/view/MainFragment.kt
@@ -77,7 +77,7 @@ class MainFragment : BaseFragment() {
         }
 
     private fun initViewModelObserver() {
-        with(mViewModel){
+        with(mViewModel) {
             onDownloadClickLiveData.observe(viewLifecycleOwner) { isClicked ->
                 if (isClicked) {
                     when {
@@ -97,11 +97,13 @@ class MainFragment : BaseFragment() {
                 }
             }
 
-            when (selectedDownloadMethodLiveData.value) {
-                Constants.DOWNLOAD_UDACITY_ID -> mBinding.udacityRadioButton.isChecked = true
-                Constants.DOWNLOAD_GLIDE_ID -> mBinding.glideRadioButton.isChecked = true
-                Constants.DOWNLOAD_RETROFIT_ID -> mBinding.retrofitRadioButton.isChecked = true
-                else -> { /* Handle other cases if necessary */
+            with(mBinding) {
+                when (selectedDownloadMethodLiveData.value) {
+                    Constants.DOWNLOAD_UDACITY_ID -> udacityRadioButton.isChecked = true
+                    Constants.DOWNLOAD_GLIDE_ID -> glideRadioButton.isChecked = true
+                    Constants.DOWNLOAD_RETROFIT_ID -> retrofitRadioButton.isChecked = true
+                    else -> { /* Handle other cases if necessary */
+                    }
                 }
             }
 
